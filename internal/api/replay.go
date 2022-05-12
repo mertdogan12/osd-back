210848ce4a394022a7993693c60a01a6b52b0736
Mert Dogan <mert.dogan@me.com>
2022-05-12 21:39:21 +0200

    checks permission and token

--- internal/api/replay.go
+++ internal/api/replay.go
@@ -1,7 +1,29 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/mertdogan12/osd-perm/pkg/helper"
+	"github.com/mertdogan12/osd/pkg/user"
+)
 
 func SaveReplay(w http.ResponseWriter, r *http.Request) {
-	checkPermission(r, "replay.save")
+	perm, err := checkPermission(r, "replay.save")
+
+	if err == user.AuthError {
+		helper.ApiRespond(http.StatusUnauthorized, "Token is invalid", w)
+		return
+	}
+
+	if err != nil {
+		helper.ApiRespondErr(err, w)
+		return
+	}
+
+	if !perm {
+		helper.ApiRespond(http.StatusUnauthorized, "No permissions", w)
+		return
+	}
+
+	helper.ApiRespond(http.StatusOK, ":)", w)
 }
