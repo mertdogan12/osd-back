c15e70b649154790e28c921911ce56af156aa3c5
Mert Dogan <mert.dogan@me.com>
2022-05-14 22:33:24 +0200

    saves the replay (not sorted)

--- internal/api/replay.go
+++ internal/api/replay.go
@@ -1,10 +1,13 @@
 package api
 
 import (
+	"io/ioutil"
 	"net/http"
+	"os"
 
 	"github.com/mertdogan12/osd-perm/pkg/helper"
 	"github.com/mertdogan12/osd/pkg/user"
+	parser "github.com/mertdogan12/osu-replay-converter/pkg/osu-replay-parser"
 )
 
 func SaveReplay(w http.ResponseWriter, r *http.Request) {
@@ -25,5 +28,21 @@ func SaveReplay(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	helper.ApiRespond(http.StatusOK, ":)", w)
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		helper.ApiRespondErr(err, w)
+		return
+	}
+
+	filepath := os.Getenv("BACK_SAVEDIR") + "/replay.osr"
+	err = os.WriteFile(filepath, body, 0644)
+	if err != nil {
+		helper.ApiRespondErr(err, w)
+		return
+	}
+
+	parsedObj, err := parser.ConvertToObject(filepath)
+
+	helper.ApiRespond(http.StatusOK, "Replay from "+parsedObj.PlayerName+" saved.", w)
 }
